80c881fa4e5581cc8f41d3e733e51f7553680619
AditiKalegaonkar <132119095+AditiKalegaonkar@users.noreply.github.com>
2025-08-05 08:17:23 +0530

    Changed the model to Gemini

--- src/RAG.py
+++ src/RAG.py
@@ -1,38 +1,42 @@
 # Libraries
 from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEndpointEmbeddings
-from langchain_huggingface import HuggingFaceEndpoint
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
-HUGGING_FACE_API_KEY = os.getenv("HUGGING_FACE_API_KEY")
-repo_id = "mistralai/Mistral-7B-Instruct-v0.3"
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+
 
 def load_and_chunk(path):
     loader = PyPDFLoader(path)
     documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=50)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=50)
     docs = text_splitter.split_documents(documents)
     return docs
 
+
 def pipeline(docs):
-    embeddings = HuggingFaceEndpointEmbeddings(model="sentence-transformers/all-mpnet-base-v2",huggingfacehub_api_token=HUGGING_FACE_API_KEY)
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
     db = FAISS.from_documents(docs, embeddings)
     print("Embedded succesfully")
-    retriever = db.as_retriever(search_type ="similarity",search_kwargs ={"k" :20})
-    llm = HuggingFaceEndpoint(repo_id=repo_id,max_length=512,temperature=0.3,huggingfacehub_api_token=HUGGING_FACE_API_KEY, task="text-generation")
+    retriever = db.as_retriever(
+        search_type="similarity", search_kwargs={"k": 20})
 
+    llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro", temperature=0.4)
     prompt_t = """Based on the provided content create a dictionary datatype of definition or important question 
     related to the context {context} and answer to the question. The answer should in the form of dictionary
-    with  key as question and value as answer to the question. Make these questions and answers only relevant 
-    to the context provided only. Only output the result in the form of dictionary. Create at least 2 questions per page. 
-    Remember to include each and every topic at least once. Dont add any question numbers only give answer in the form of dictionary datatype in json.
+    with key as question and value as answer to the question. Make these questions and answers only relevant 
+    to the context provided only. Only output the result in the form of dictionary. Create at least 2 questions per page.
+    Remember to include each and every topic at least once. Osnly give Question answer in the form of dictionary datatype in json.
     """
-   
+
     prompt = PromptTemplate(
         template=prompt_t,
         input_variables=["context"],
@@ -47,6 +51,7 @@ def pipeline(docs):
         chain_type_kwargs={"prompt": prompt}
     )
 
-    result = retrievalQA.invoke("Create a dictionary datatype of important definitions and questions as a key and answer as value.")
+    result = retrievalQA.invoke(
+        "Create a dictionary datatype of important definitions and questions as a key and answer as value.")
 
     return result['result']
